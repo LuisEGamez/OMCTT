8a03e6719e81e4100fc921e1a0954abee57df7e4
LuisEGamez <luisgamezdvp@gmail.com>
2022-09-30 15:53:09 +0200

    adding project

---
+++ src/main/java/com/omctt/OmcttApplication.java
@@ -0,0 +1,13 @@
+package com.omctt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmcttApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmcttApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/omctt/OmcttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omctt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmcttApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
